1f14924a4e5ce6d087559b99430b75753bca00c0
prince-372 <pmor18314@gmail.com>
2023-11-25 15:32:14 +0530

    update

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,3 +1,7 @@
+let redact = document.querySelector(".redact")
+let extract = document.querySelector(".extract")
+let detect = document.querySelector(".detect")
+let compare = document.querySelector(".compare")
 function backtop() {
     window.scrollTo(0, 0);
 }
@@ -10,56 +14,30 @@ window.addEventListener("scroll", function () {
         mybackto.style.display = "none";
     }
 });
-
-let a = document.getElementById("nav-bar");
-let flow = document.querySelector(".sec-flow");
-let cross4 = document.querySelector(".cross-4");
-let cross5 = document.querySelector(".cross-5");
-let cross6 = document.querySelector(".cross-6");
-let menuIcon = document.querySelector("#menuIcon");
-let home = document.querySelector(".home");
-let about = document.querySelector(".about");
-let services = document.querySelector(".services");
-let pricing = document.querySelector(".pricing");
-let faq = document.querySelector(".faq");
-
-menuIcon.addEventListener("click", function () {
-    if (a.classList !== document.querySelector("fix-lft")) {
-        a.classList.toggle("fix-lft");
-        flow.classList.toggle("flow-hidden");
-        cross4.classList.toggle("cross-1");
-        cross5.classList.toggle("cross-2");
-        cross6.classList.toggle("cross-3")
+//nav-bar
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
     }
-});
-
-home.addEventListener("click", function () {
-    a.classList.toggle("fix-lft");
-    flow.classList.remove("flow-hidden");
-    cross4.classList.toggle("cross-1");
-    cross5.classList.toggle("cross-2");
-    cross6.classList.toggle("cross-3");
-});
-about.addEventListener("click", function () {
-    a.classList.remove("fix-lft");
-    flow.classList.remove("flow-hidden");
-    cross4.classList.toggle("cross-1");
-    cross5.classList.toggle("cross-2");
-    cross6.classList.toggle("cross-3");
-}); 
-services.addEventListener("click", function () {
-    a.classList.remove("fix-lft");
-    flow.classList.remove("flow-hidden");
-    cross4.classList.toggle("cross-1");
-    cross5.classList.toggle("cross-2");
-    cross6.classList.toggle("cross-3");
-});
-pricing.addEventListener("click", function () {
-    a.classList.remove("fix-lft");
-    flow.classList.remove("flow-hidden");
-    cross4.classList.toggle("cross-1");
-    cross5.classList.toggle("cross-2");
-    cross6.classList.toggle("cross-3");
-});
-
-
+    redact.addEventListener("click",function(){
+        document.getElementById("navbar").classList.remove("start-0");
+        document.body.classList.remove("overflow_hidden");
+        document.querySelector(".menu").classList.remove("cross")
+    })
+    extract.addEventListener("click",function(){
+        document.getElementById("navbar").classList.remove("start-0");
+        document.body.classList.remove("overflow_hidden");
+        document.querySelector(".menu").classList.remove("cross")
+    })
+    detect.addEventListener("click",function(){
+        document.getElementById("navbar").classList.remove("start-0");
+        document.body.classList.remove("overflow_hidden");
+        document.querySelector(".menu").classList.remove("cross")
+    })
+    compare.addEventListener("click",function(){
+        document.getElementById("navbar").classList.remove("start-0");
+        document.body.classList.remove("overflow_hidden");
+        document.querySelector(".menu").classList.remove("cross")
+    })
+    
